5e3df4987888ebff1497b100d3e4c4c9a71f9c5b
divythakur <divyanshuthakur75@gmail.com>
2023-10-01 23:28:11 +0530

    debug cors issue

--- src/index.js
+++ src/index.js
@@ -8,7 +8,16 @@ const express = require("express");
 
 const app = express();
 app.set("view engine", "hbs");
+
 app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 
 const uri =process.env.MONGODB_URL;
 
