b9f26a820c9bd494bb6f02b8081bb05e159c5e77
divythakur <divyanshuthakur75@gmail.com>
2023-10-01 23:58:35 +0530

    debug env variables

--- src/index.js
+++ src/index.js
@@ -1,25 +1,33 @@
 const mongoose = require("mongoose");
 const cors = require("cors");
- const bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 const express = require("express");
- const path = require("path");
- const {registerUserFunc,loginUserFunc, listofSchools} =require('./handlers')
- require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
+const path = require("path");
+const {
+  registerUserFunc,
+  loginUserFunc,
+  listofSchools,
+} = require("./handlers");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 
 const app = express();
 app.set("view engine", "hbs");
 
 app.use(cors());
+const PORT =process.env.PORT || 80
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
   next();
 });
 
-const uri =process.env.MONGODB_URL;
+const uri = process.env.MONGODB_URL;
 
 mongoose.connect(uri, {
   useNewUrlParser: true,
@@ -30,19 +38,8 @@ app.get("/listItems", listofSchools);
 
 app.post("/login", bodyParser.json(), loginUserFunc);
 
+app.post("/register", bodyParser.json(), registerUserFunc);
 
-app.post("/register", bodyParser.json(),registerUserFunc);
-
-
- 
-app.listen(process.env.PORT, () => console.log(`Example app listening on port}!`));
-
- 
- 
-
- 
- 
-
-
-
-
+app.listen(PORT, () =>
+  console.log(`Example app listening on ${PORT}!`)
+);
